04e9fcf46cee25e4170fc12140dc6ef01984010d
softmattertheory <timothy.atherton@tufts.edu>
2024-12-28 10:44:07 -0500

    Initialize curl library

--- src/curl.c
+++ src/curl.c
@@ -5,13 +5,18 @@
 */
 
 #include "curl.h"
+#include <curl/curl.h>
+
+CURL *curl;
 
 /* -------------------------------------------------------
  * Initialization and finalization
  * ------------------------------------------------------- */
 
 void curl_initialize(void) { 
+    curl_global_init(CURL_GLOBAL_ALL);
 }
 
 void curl_finalize(void) { 
+    curl_global_cleanup();
 }
