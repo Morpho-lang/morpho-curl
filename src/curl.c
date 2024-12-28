c69f9ad435315b372fabc4a33b6e90c45cb35bc9
softmattertheory <timothy.atherton@tufts.edu>
2024-12-28 10:15:46 -0500

    Commit initial files

---
+++ src/curl.c
@@ -0,0 +1,17 @@
+/** @file curl.c
+ *  @author T J Atherton
+ *
+ *  @brief Wrapper for cURL library
+*/
+
+#include "curl.h"
+
+/* -------------------------------------------------------
+ * Initialization and finalization
+ * ------------------------------------------------------- */
+
+void curl_initialize(void) { 
+}
+
+void curl_finalize(void) { 
+}
---
+++ src/curl.h
@@ -0,0 +1,11 @@
+/** @file curl.h
+ *  @author T J Atherton
+ *
+ *  @brief Wrapper for cURL library
+*/
+
+#ifndef curl_h
+#define curl_h
+
+
+#endif
